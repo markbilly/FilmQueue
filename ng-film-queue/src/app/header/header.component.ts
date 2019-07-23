595d3f2cb84b61009f255d05365dcd78c7129cf7
Mark Richards <mark.richards@ecclesiastical.com>
2019-07-23 15:03:21 +0100

    make adding a film page more like a dismissable modal

--- ng-film-queue/src/app/header/header.component.ts
+++ ng-film-queue/src/app/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../core/auth/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
+import * as _ from 'lodash';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -11,8 +15,20 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   isAuthenticated: boolean;
   subscription: Subscription;
+  mode: "page" | "modal";
+  faTimes = faTimes;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private location: Location, router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.mode = this.determineMode(event.url);
+      }
+    });
+  }
+
+  private determineMode(url: string): "page" | "modal" {
+    return _.includes(url, "watchlist") ? "modal" : "page";
+  }
 
   ngOnInit() {
     this.subscription = this.authService.authNavStatus$.subscribe(status => this.isAuthenticated = status);
